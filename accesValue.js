a8ca5756c866c1b0709adc74e33f6ae7a98cd703
Rohul1 <rohul669319@gmail.com>
2023-10-20 21:52:26 +0600

    first commit

---
+++ accesValue.js
@@ -0,0 +1,22 @@
+const students = [
+    { name: "Rohul", age: 16, role: "CO CR" },
+    { name: "asha", age: 17, role: " CR" },
+    { name: "joy", age: 18, role: "student" },
+    { name: "Mehedi", age: 19, role: "brilliant" }
+];
+console.log(students[0].role);
+
+
+
+
+const developer = {
+    name: "rohul",
+    age: 22,
+    company: {
+        companyName: "Talpotro Foundation",
+        address: "Bagmara, Rajshahi",
+        CEO: "Sharif uddin",
+        Partner: "Rohul amin"
+    }
+}
+console.log(developer.company.CEO);
